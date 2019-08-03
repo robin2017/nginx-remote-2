e4558aeb7f41baec3707ba43b815dda1f5beda1b
robin2017 <robin.seu@foxmail.com>
2019-08-03 11:12:55 +0800

    tmp

--- server/secret.sh
+++ server/secret.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#
+#变量的=前后不能加空格
 servername="68.168.137.218:8060"
 download_file="/download/file.img"
 time_num=$(date  -d "2019-9-28 00:00:00" +%s)
-secret_num="myKey"
+secret_num="robin"
 
 res=$(echo -n "${time_num}${download_file} ${secret_num}"|openssl md5 -binary | openssl base64 | tr +/ -_ | tr -d =)
 
