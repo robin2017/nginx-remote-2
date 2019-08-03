14f92f7207a33e6b7b56af7ed22ee6b0132ac587
robin2017 <robin.seu@foxmail.com>
2019-08-03 11:38:51 +0800

    安装geoip

--- server/secret.sh
+++ server/secret.sh
@@ -4,7 +4,5 @@ servername="68.168.137.218:8060"
 download_file="/download/file.img"
 time_num=$(date  -d "2019-9-28 00:00:00" +%s)
 secret_num="robin"
-
 res=$(echo -n "${time_num}${download_file} ${secret_num}"|openssl md5 -binary | openssl base64 | tr +/ -_ | tr -d =)
-
 echo "http://${servername}${download_file}?md5=${res}&expires=${time_num}"
